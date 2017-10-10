761b06c641dbe90f4c09939fd5f03264272f9363
Oliver Eriksson <virus.oliver@gmail.com>
2017-10-10 15:19:53 +0200

    orderby 1.0

---
+++ LINQShop/LINQShop/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQShop
+{
+    class Program
+    {
+        private static void Menu()
+        {
+            char c = 'C';
+            {
+                while (c != 'x')
+                {
+                    Console.WriteLine
+                        (
+                        "Press 1 to sort by Price \nPress 2 to sort by Name \nPress 3 to sort by Price then Name \nPress 4 to sort by Price, grouped by Category \nPress 5 to search by Name \nPress 6 to search by Price \nPress 7 to search by Price and Name \nPress 8 to search by Price OR Name\nPress 0 to to quit."
+                        );
+
+                    string input = Console.ReadLine();
+                    c = input[0];
+
+                    switch (c)
+                    {
+                        case '1':
+                            ShopHandler.PriceSort();
+                            Console.WriteLine();
+                            break;
+                        case '2':
+                            ShopHandler.NameSort();
+                            Console.WriteLine();
+                            break;
+                        case '3':
+                            ShopHandler.PNameSort();
+                            Console.WriteLine();
+                            break;
+                        case '4':
+                            ShopHandler.CatPriceSort();
+                            Console.WriteLine();
+                            break;
+                        case '0':
+                            return;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           
+            //Create a menu and make 
+            Menu();
+            //calls to ShopHandler for search and sort. 
+            Console.ReadKey();
+        }
+    }
+
+    
+    /*  
+     Enumerations are special sets of named values 
+     which all maps to a set of numbers, usually integers. 
+     They come in handy when you wish to be able to choose 
+     between a set of constant values. 
+     Category is here used to define category for Items.
+    */
+    public enum Category
+    {
+        Foods,
+        Drinks,
+        Bread,
+        Books,
+        Sport
+    }
+}
+//var ps = new ShopHandler();
---
+++ LINQShop/LINQShop/ShopHandler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQShop
+{
+    class ShopHandler 
+    {
+        private ItemInventory inventory;
+
+        //constructor
+        public ShopHandler()
+        {
+            inventory = new ItemInventory();
+        }
+        
+        //Add methods to: 
+        public static void PriceSort()
+        {
+            var inventory = new ItemInventory().GetAllItems();
+
+            var HoldItem =
+                from i in inventory
+                orderby i.Price descending
+                select i;
+
+            foreach (Item i in HoldItem)
+            {
+                Console.WriteLine("Price: {1}\t Category: {2}\t Name: {0}", i.Name, i.Price, i.Cat);
+            }
+        }
+
+        public static void NameSort()
+        {
+            var inventory = new ItemInventory().GetAllItems();
+
+            var HoldItem =
+                from i in inventory
+                orderby i.Name
+                select i;
+
+            foreach (Item i in HoldItem)
+            {
+                Console.WriteLine("Price: {1}\t Category: {2}\t Name: {0}", i.Name, i.Price, i.Cat);
+            }
+        }
+
+        public static void PNameSort()
+        {
+            var inventory = new ItemInventory().GetAllItems();
+
+            var HoldItem =
+                from i in inventory
+                orderby i.Name
+                orderby i.Price
+                select i;
+
+            foreach (Item i in HoldItem)
+            {
+                Console.WriteLine("Price: {1}\t Category: {2}\t Name: {0}", i.Name, i.Price, i.Cat);
+            }
+        }
+
+        public static void CatPriceSort()
+        {
+            var inventory = new ItemInventory().GetAllItems();
+
+            var HoldItem =
+                from i in inventory
+                //group by i.Cat
+                orderby i.Price
+                orderby i.Cat
+ 
+                select i;
+           
+            foreach (Item i in HoldItem)
+            {
+                Console.WriteLine("Price: {1}\t Category: {2}\t Name: {0}", i.Name, i.Price, i.Cat);
+            }
+        }
+        //return items sorted by name/price and name/price grouped by category      done\price
+        //Also to search based on name, price or both at the same time.
+        //Use LINQ to query/filter/order/group the items.
+
+
+
+    }
+}
